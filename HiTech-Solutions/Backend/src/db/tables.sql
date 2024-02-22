eda77be9e876f77d91ab091f556b8fa888a16dc5
HaAymar <he201905@students.ephec.be>
2024-02-22 16:05:57 +0100

    Renewing the database

--- HiTech-Solutions/Backend/src/db/tables.sql
+++ HiTech-Solutions/Backend/src/db/tables.sql
@@ -1,44 +1,69 @@
+-- Création de la table Roles
+
+--- Add "AUTO_INCREMENT" in primary Key ---
+CREATE TABLE Roles (
+    Id INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(255) NOT NULL
+);
+
+-- Création de la table Sections
+CREATE TABLE Sections (
+    Id INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(255) NOT NULL
+);
+
+-- Création de la table Users
 CREATE TABLE Users (
-    Id INT PRIMARY KEY,
-    Nom VARCHAR(255),
-    Prenom VARCHAR(255),
-    Email VARCHAR(255),
-    Role VARCHAR(255),
-    Tel VARCHAR(255),
-    Section VARCHAR(255),
+    Id INT AUTO_INCREMENT PRIMARY KEY,
+    Id_Role INT,
+    Id_Section INT,
+    Name VARCHAR(255) NOT NULL,
+    Surname VARCHAR(255) NOT NULL,
+    Email VARCHAR(255) NOT NULL,
+    Tel VARCHAR(20),
     DateInscription DATE,
     DateDeFin DATE,
-    Password VARCHAR(255),
-    Fk_Id_Role INT,
-    Fk_Id_Section INT,
-    FOREIGN KEY (Fk_Id_Role) REFERENCES Roles(Id),
-    FOREIGN KEY (Fk_Id_Section) REFERENCES Section(Id)
+    Password VARCHAR(255) NOT NULL,
+    FOREIGN KEY (Id_Role) REFERENCES Roles(Id),
+    FOREIGN KEY (Id_Section) REFERENCES Sections(Id)
 );
 
-CREATE TABLE Roles (
-    Id INT PRIMARY KEY,
-    Name VARCHAR(255)
+-- Création de la table Courses
+CREATE TABLE Courses (
+    Id INT AUTO_INCREMENT PRIMARY KEY,
+    Id_Section INT,
+    Name VARCHAR(255) NOT NULL,
+    FOREIGN KEY (Id_Section) REFERENCES Sections(Id)
 );
 
-CREATE TABLE Section (
-    Id INT PRIMARY KEY,
-    Name VARCHAR(255)
-);
-
+-- Création de la table Services
 CREATE TABLE Services (
-    Id INT PRIMARY KEY,
-    Name VARCHAR(255),
+    Id INT AUTO_INCREMENT PRIMARY KEY,
+    Id_User INT,
+    Name VARCHAR(255) NOT NULL,
     Description TEXT,
     Photo VARCHAR(255),
-    Id_Admin INT,
-    Fk_Id_Roles INT,
-    FOREIGN KEY (Fk_Id_Roles) REFERENCES Roles(Id)
+    FOREIGN KEY (Id_User) REFERENCES Users(Id)
 );
 
-CREATE TABLE Courses (
-    Id INT PRIMARY KEY,
-    Id_Section INT,
-    Name VARCHAR(255),
-    Fk_Id_Section INT,
-    FOREIGN KEY (Fk_Id_Section) REFERENCES Section(Id)
+-- Création de la table TeachersCourses
+CREATE TABLE TeachersCourses (
+    Id INT AUTO_INCREMENT PRIMARY KEY,
+    Id_course INT,
+    Id_User INT,
+    FOREIGN KEY (Id_course) REFERENCES Courses(Id),
+    FOREIGN KEY (Id_User) REFERENCES Users(Id)
+);
+
+-- Création de la table CreationTests
+CREATE TABLE CreationTests (
+    Id INT AUTO_INCREMENT PRIMARY KEY,
+    Id_User INT,
+    Id_course INT,
+    Name VARCHAR(255) NOT NULL,
+    Description TEXT,
+    DateTest DATE,
+    Validation ENUM('Yes', 'No'),
+    FOREIGN KEY (Id_User) REFERENCES Users(Id),
+    FOREIGN KEY (Id_course) REFERENCES Courses(Id)
 );
