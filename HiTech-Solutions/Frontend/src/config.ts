40814947cc7e66b91c8c093dccbc4fb642d011b5
HaAymar <he201905@students.ephec.be>
2024-05-13 14:54:21 +0200

    update config.ts

--- HiTech-Solutions/Frontend/src/config.ts
+++ HiTech-Solutions/Frontend/src/config.ts
@@ -1,4 +1,4 @@
 export const BE_URL =
 	process.env.NODE_ENV === "production"
-		? "https://hitech-backend.azurewebsites.net/api/"
+		? "https://hitech-backend.azurewebsites.net:3001/api/"
 		: "http://localhost:3001/api/";
